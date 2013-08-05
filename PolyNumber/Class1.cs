99b48b15598685ecc2f657cc324df3c6a7fc5404
Craig Gidney <craig.gidney@gmail.com>
2013-08-05 13:14:59 -0300

    Representing a number as a polynomial

---
+++ PolyNumber/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolyNumber
+{
+    public class Class1
+    {
+    }
+}
